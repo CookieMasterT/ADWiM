0445027be617c9a7e960d80d98bf3607e0b94a78
CookieMasterT <124045269+CookieMasterT@users.noreply.github.com>
2025-02-06 13:38:41 +0100

    a

--- adwim/src/Content.js
+++ adwim/src/Content.js
@@ -1,36 +1,49 @@
-function Content()
-{
-    function handle_name_change()
-    {
-        const names = ['Bob','Kevin','Dave']
-        const int = Math.floor(Math.random() * 3)
-        return names[int]
-    }
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
-    function handle_click()
+const Content = () => {
+  const [items, setItems] = useState([
     {
-        console.log('You clicked it');
-    }
-
-    function handle_click_v2(name)
+      id: 1,
+      checked: false,
+      item: "amongus",
+    },
     {
-        console.log(name);
-    }
-
-    function handle_click_v3(a)
+      id: 2,
+      checked: false,
+      item: "amongus",
+    },
     {
-        console.log(a.target.innerText);
-    }
-    return(
-        <main>
-            <p>
-                Hello {handle_name_change()}!
-            </p>
-            <button onClick={handle_click}>Click Here</button>
-            <button onClick={() => handle_click_v2('Dave')}>Click Here Too</button>
-            <button onClick={(a) => handle_click_v3(a)}>Click Here Too</button>
-        </main>
-    )    
-}
+      id: 3,
+      checked: true,
+      item: "amongus",
+    },
+    {
+      id: 4,
+      checked: false,
+      item: "amongus",
+    },
+  ]);
 
-export default Content
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
+    setItems(listItems);
+    localStorage.setItem()
+  };
+
+  return (
+    <main>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default Content;
