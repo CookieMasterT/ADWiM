b6bf5fda441d555e0d44700e3e391a71c2d8028d
CookieMasterT <124045269+CookieMasterT@users.noreply.github.com>
2025-02-12 15:14:56 +0100

    finish lesson 7
    a

--- adwim/src/Content.js
+++ adwim/src/Content.js
@@ -28,22 +28,32 @@ const Content = () => {
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
     setItems(listItems);
-    localStorage.setItem()
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
   };
 
   return (
     <main>
+    {items.length ? (
       <ul>
         {items.map((item) => (
           <li className="item" key={item.id}>
             <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
-            <label>{item.item}</label>
-            <FaTrashAlt role="button" tabIndex="0" />
+            <label onDoubleClick={() => handleCheck(item.id)} style={(item.checked) ? {textDecoration: 'line-through'} : null}>{item.item}</label>
+            <FaTrashAlt onClick={() => handleDelete(item.id)} role="button" tabIndex="0" />
           </li>
         ))}
       </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )};
     </main>
-  );
+  )
 };
 
 export default Content;
