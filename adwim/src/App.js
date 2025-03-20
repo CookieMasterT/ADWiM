26d443f72f1e837c2693caac9637e03b66a5fdb8
CookieMasterT <124045269+CookieMasterT@users.noreply.github.com>
2025-03-20 13:22:19 +0100

    Add files via upload

--- adwim/src/App.js
+++ adwim/src/App.js
@@ -3,33 +3,50 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
-  const [newItem, setNewItem] = useState('')
-  const [search, setSearch] = useState('')
+  //npx json-server@0.17.4 -p 3500 -w db.json
+  const API_URL = 'http://localhost:3500/items';
+  
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+  const [search, setSearch] = useState('');
+  const [fetchError, setFetchError] = useState(null);
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-  }
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive excepted data')
+        const listItems = await response.json();
+        console.log(listItems);
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      }
+      
+    }
+    fetchItems();
+  }, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    setItems(listItems);
   }
 
   const handleSubmit = (e) => {
@@ -51,11 +68,14 @@ function App() {
         search={search}
         setSearch={setSearch}
       />
-      <Content
+      <main>
+        {fetchError && <p> style={{ color: "red" }}{`Error: ${fetchError}`}</p>}
+      {!fetchError && <Content
         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
-      />
+      />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
